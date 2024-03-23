3df3b67dc244cff35b78d4e87b7d68a3877581e4
Jeonggon Kim <kjjkjykjk@gmail.com>
2024-03-23 21:11:25 +0900

    admin.py register

--- tests/admin.py
+++ tests/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.apps import apps
+from .models import *
+
+app = apps.get_app_config('tests')
+
+# 모든 모델을 반복하여 admin 사이트에 등록
+for model_name, model in app.models.items():
+    admin.site.register(model)
 
 # Register your models here.
