aca62752273efd88d110fdf2e9b14b66d90248ce
hbam1 <hbbaamm@gmail.com>
2024-03-23 22:05:44 +0900

    [feat] gunicorn 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    'django_elasticsearch_dsl',
+    "rest_framework_simplejwt",
     "users",
     "tests"
 ]
