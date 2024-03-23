d59d7b469a77ce550a57ab86c5f11ec4a63a8dd9
hbam1 <hbbaamm@gmail.com>
2024-03-24 00:52:58 +0900

    fix

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login/", views.LoginAPIView),
+    path("login/", views.LoginAPIView.as_view()),
     path("signup/", views.SignUpAPIView.as_view()),
 ]
