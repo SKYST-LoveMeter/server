47b89dda369366ae228bfb1c87b7e0f1e7d9ce86
Jeonggon Kim <kjjkjykjk@gmail.com>
2024-03-24 02:14:30 +0900

    완료

--- tests/urls.py
+++ tests/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "tests"
 
 urlpatterns = [
-    path("", views.start_test, name="start_test"), 
-    path("<int:test_id>/result", views.test_result, name="test_result"),
-    path("love_category/", views.LoveCategory.as_view())
+    path("", views.StartTestAPIView.as_view(), name="start_test"), 
+    path("<int:test_id>/result", views.TestResultAPIView.as_view(), name="test_result"),
+    path("love_category/", views.LoveCategoryCreate.as_view())
 ] 
--- tests/views.py
+++ tests/views.py
@@ -5,60 +5,62 @@ from .serializers import *
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
+from .models import *
 
-@api_view(['Get'])
-def start_test (request):
-    queryset = LoveCategory.objects.all()
-    serializer = LoveCategorySerializer(queryset, many=True)
-    data = {item['id']: item['name'] for item in serializer.data}
-    #test = Test.objects.create(user= request.user)
-    context = {
-        "category" : data
-        #"test_id" : test.id
-    }
-    return Response(context)
+class StartTestAPIView(APIView):
+    def get (self,request):
+        queryset = LoveCategory.objects.all()
+        print(queryset)
+        serializer = LoveCategorySerializer(queryset, many=True)
+        data = {item['id']: item['name'] for item in serializer.data}
+        test = Test.objects.create(user= request.user)
+        context = {
+            "category" : data,
+            "test_id" : test.id
+        }
+        return Response(context)
    
 
-@api_view(['Post'])
-def test_result (request, test_id):
-    received_data = request.data
-    loves = received_data['love']
-    efforts = received_data['effort']
+class TestResultAPIView(APIView):
+    def post (request, test_id):
+        received_data = request.data
+        loves = received_data['love']
+        efforts = received_data['effort']
 
-    made_test = Test.objects.get(id= test_id)
+        made_test = Test.objects.get(id= test_id)
 
-    love_id_list = []
-    for love in loves :
-        category = LoveCategory.objects.get(id=love['id'])
-        made_love = Love.objects.create(name = category, prediction = love['percentage'])
-        made_test.loves.add(made_love)
-        love_id_list.append(made_love.id)
-   
-    total = 0
-    for effort in efforts :
-        made_effort = Effort.objects.create(description= effort['description'], test = made_test, value=effort['value']) 
-        for lover in effort['lovers'] : 
-            love = Love.objects.get(id = love_id_list[lover-1])
-            love.efforts.add(made_effort)
-        total = total + effort['value'] * len(effort['lovers'])
+        love_id_list = []
+        for love in loves :
+            category = LoveCategory.objects.get(id=love['id'])
+            made_love = Love.objects.create(name = category, prediction = love['percentage'])
+            made_test.loves.add(made_love)
+            love_id_list.append(made_love.id)
+    
+        total = 0
+        for effort in efforts :
+            made_effort = Effort.objects.create(description= effort['description'], test = made_test, value=effort['value']) 
+            for lover in effort['lovers'] : 
+                love = Love.objects.get(id = love_id_list[lover-1])
+                love.efforts.add(made_effort)
+            total = total + effort['value'] * len(effort['lovers'])
 
-    for love_id in love_id_list :
-        love = Love.objects.get(id = love_id)
-        tmp = 0 
-        for effort in love.efforts.all() :
-            tmp = tmp + effort.value
-        love.result = tmp/total * 100
-        love.save()
+        for love_id in love_id_list :
+            love = Love.objects.get(id = love_id)
+            tmp = 0 
+            for effort in love.efforts.all() :
+                tmp = tmp + effort.value
+            love.result = tmp/total * 100
+            love.save()
 
-    serializer = LoveSerializer(made_test.loves.all(), many=True)
-    context = [
-            {"name": LoveCategory.objects.get(id=item['name']).name, "percentage": item['result']}
-            for item in serializer.data
-        ]
+        serializer = LoveSerializer(made_test.loves.all(), many=True)
+        context = [
+                {"name": LoveCategory.objects.get(id=item['name']).name, "percentage": item['result']}
+                for item in serializer.data
+            ]
 
-    return Response(context)    
+        return Response(context)    
 
-class LoveCategory(APIView):
+class LoveCategoryCreate(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
