1406c37f41858ecfcf7ccad9ecd92e0bcc56d6e9
Jeonggon Kim <kjjkjykjk@gmail.com>
2024-03-23 21:01:29 +0900

    초기세팅 완료

--- tests/views.py
+++ tests/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import *
+from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def start_test (request):
