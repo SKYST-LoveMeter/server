95dbe8c5e3c5e1740aa12bb68985e3aad529b305
Jeonggon Kim <kjjkjykjk@gmail.com>
2024-03-23 15:15:27 +0900

    #2 feat: tests app model 생성

--- tests/models.py
+++ tests/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class Test(models.Model) :
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  loves = models.ManyToManyField('Love')
+
+class Effort(models.Model) :
+  description = models.CharField(max_length=1000, null= False)
+  # test = models.ForeignKey(Test, on_delete=models.CASCADE)
+
+class Love(models.Model) : 
+  name = models.CharField(max_length=1000, null= False)
+  prediction = models.PositiveIntegerField(default=0)
+  result = models.PositiveIntegerField(default=0)
+  efforts = models.ManyToManyField(Effort)
+
+class Favorite(models.Model) : 
+  name = models.CharField(max_length=1000, null= False)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
