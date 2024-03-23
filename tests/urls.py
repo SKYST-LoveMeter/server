d8a7538b9e142be94685cd445c95a6923c6df0d0
Jeonggon Kim <kjjkjykjk@gmail.com>
2024-03-24 04:55:32 +0900

    조회 함수

--- tests/urls.py
+++ tests/urls.py
@@ -6,5 +6,6 @@ app_name = "tests"
 urlpatterns = [
     path("", views.StartTestAPIView.as_view(), name="start_test"), 
     path("<int:test_id>/result", views.TestResultAPIView.as_view(), name="test_result"),
+    path("<int:test_id>/result_view", views.TestResultViewAPIView.as_view(), name="test_result"),
     path("love_category/", views.LoveCategoryCreate.as_view())
 ] 
